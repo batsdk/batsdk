e2452e6b73d79fa024a1e37b6443da747068dfea
Shemil <shemil@synrgy.us>
2024-10-20 18:39:31 +0530

    UI: Mobile responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ function App() {
   return (
     <section className="bg-[#ededed] min-h-screen min-w-[100vw]">
       <div className="flex min-h-screen flex-col justify-center items-center text-[#231f20]">
-        <h1 className="text-9xl" >Shemil</h1>
-        <h5 className="text-2xl" >Engineer Crafting innovative, scalable solutions</h5>
+        <h1 className="text-7xl md:text-9xl" >Shemil</h1>
+        <h5 className="text-lg md:text-2xl flex flex-wrap text-center" >Engineer Crafting innovative, scalable solutions</h5>
       </div>
     </section>
   )
