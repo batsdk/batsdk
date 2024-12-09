c62ad192cc631d2c1496cfeafca17d8a05ba7a6e
Shemil <shemil@synrgy.us>
2024-12-09 23:26:26 +0530

    Changed few things

--- src/App.tsx
+++ src/App.tsx
@@ -20,29 +20,11 @@ function App() {
           {/* About Section */}
           <div className="mt-10" >
             <h4 className="text-base md:text-lg">
-              Hey there! I’m {' '}
-              <span className="font-bold">
-                Shemil Kaweesha
-              </span> , a code-wrangling, guitar-strumming Software Engineer. When I’m not deep into {' '}
-              <span className="font-bold">
-                TypeScript
-              </span> {' '}
-              or figuring out why my {' '}
-              <span className="font-bold" >
-                React
-              </span> {' '}
-              app won’t render (again), you’ll find me jamming out on my {' '}
-              <span className="font-bold" >guitar</span>
-              , pretending I’m the next rock star — minus the world tour.
+              Hey there! I’m <span className="font-bold">Shemil Kaweesha</span>, a code-wrangling, guitar-strumming Software Engineer. When I’m not deep into <span className="font-bold">TypeScript</span> or figuring out why my <span className="font-bold">React</span> app won’t render (again), you’ll find me unwinding with my guitar, enjoying the creative flow.
             </h4>
             <br />
             <h4 className="text-base md:text-lg" >
-              I speak fluent <span className="font-bold" >TypeScript</span>{''}
-               a bit of {' '}
-              <span className="font-bold" >Golang</span>{''}
-              , and sarcasm (the third one is optional, but it makes debugging more fun).
-              I love clean code, smart design, and making the internet a little better, one project at a time.
-              And no, I don’t run on coffee like the stereotypical dev — I’m more of a “give me good vibes and snacks” kind of guy. Let’s build something awesome together — or at least laugh at my code.
+            I speak fluent <span className="font-bold">TypeScript</span>, dabble in cloud wizardry with <span className="font-bold">Azure ⚡</span>, and occasionally throw in some sarcasm (the third one is optional, but it makes debugging more fun). I love clean code, smart design, and making the internet a little better, one project at a time. And no, I don’t run on coffee like the stereotypical dev — I’m more of a “give me good vibes and snacks” kind of guy. Let’s build something awesome together — or at least laugh at my code.
             </h4>
           </div>
 
